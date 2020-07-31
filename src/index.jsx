a18be1038dc57cc21856ad36cb1d12e30baa1f09
Andrey Kostenko <makelesscode@gmail.com>
2020-07-31 14:50:11 +0300

    Add React, Babel, eslint

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page from './components/common/Page';
+
+ReactDOM.render(<Page />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,34 @@
  * Webpack configuration
  */
 
-const path = require('path');
+const path = require("path");
 
-const mode = process.env.mode || 'development';
+const paths = {
+    js: [
+        path.resolve(__dirname, "./src/components"),
+        path.resolve(__dirname, "./src/containers"),
+        path.resolve(__dirname, "./src/actions"),
+        path.resolve(__dirname, "./src/reducers"),
+        path.resolve(__dirname, "./src/helpers"),
+        path.resolve(__dirname, "./src/index.jsx"),
+    ],
+    styles: [
+        path.resolve(__dirname, "./src/styles/**/.css"),
+        path.resolve(__dirname, "./src/styles/**/.scss")
+    ]
+};
 
+/**
+ * Environment mode
+ */
+const mode = process.env.NODE_ENV || "development";
 module.exports = {
     mode,
-    entry: './src/index.js',
+    entry: "./src/index.jsx",
     output: {
-        path: path.resolve(__dirname, './app/'),
-        publicPath: '/',
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, "./app/"),
+        publicPath: "/",
+        filename: "bundle.js"
     },
     devServer: {
         compress: true,
@@ -20,4 +37,44 @@ module.exports = {
         port: 3000,
         contentBase: path.join(__dirname, "./app/"),
     },
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: paths.js,
+                enforce: "pre",
+                loader: require.resolve("eslint-loader"),
+                options: {
+                    cache: true,
+                    eslintPath: require.resolve("eslint")
+                },
+            },
+            {
+                test: /\.jsx?$/,
+                include: paths.js,
+                loader: require.resolve("babel-loader"),
+                options: {
+                    presets: [
+                        "@babel/preset-react",
+                        "@babel/preset-env"
+                    ],
+                    cacheDirectory: true, // caching for faster rebuilds (babel-loader)
+                    cacheCompression: false,
+                    compact: mode === "production",
+                }
+            },
+            {
+                test: /\.s?css/,
+                include: paths.styles,
+                loader: require.resolve('sass-loader'),
+                options: {
+                    sourceMap: mode === "development"
+                }
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
 };
