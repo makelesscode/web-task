57c2e1a6cb993d91e6dc93d4cae25a3b66f275c6
Andrey Kostenko <makelesscode@gmail.com>
2020-08-02 16:50:30 +0300

    Add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+function Button(props) {
+  const {
+    component: Component,
+    children,
+    className,
+    onClick,
+    type,
+    outline,
+    size,
+  } = props;
+  return (
+    <Component
+      className={cx(
+        'btn',
+        `btn-${(outline && 'outline-') + type}`,
+        size && `btn-${size}`,
+        className,
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </Component>
+  );
+}
+
+Button.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.string, PropTypes.elementType]),
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf([
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'dark',
+    'light',
+  ]),
+  outline: PropTypes.bool,
+  size: PropTypes.oneOf(['sm', 'lg']),
+};
+
+Button.defaultProps = {
+  component: 'button',
+  className: '',
+  type: 'secondary',
+  onClick: null,
+  outline: false,
+  size: null,
+};
+
+export default Button;
