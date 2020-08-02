5cb2311d365e01c78ff7aca838504db75767f4f2
Andrey Kostenko <makelesscode@gmail.com>
2020-08-02 09:53:22 +0300

    Add common functions used to display errors, parse request URI and body, connect to app json db

---
+++ app/core/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+function display_api_error($error_no, $error_descr, $http_response_code = 500) {
+    http_response_code($http_response_code);
+
+    echo json_encode([
+        'code' => $error_no,
+        'description' => $error_descr,
+    ]);
+
+    exit;
+}
+
+function get_path_segments($path, $segments_to_omit = 0) {
+    $path = trim($path, '/');
+    $path_segs = explode('/', $path);
+    if ($segments_to_omit > 0) {
+        array_splice($path_segs, 0, $segments_to_omit);
+    }
+
+    return $path_segs;
+}
+
+function retrieve_webdav_json() {
+    $data = file_get_contents('php://input');
+    try {
+        return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
+    } catch (JsonException $e) {
+        display_api_error(415, 'Unknown format, JSON expected.', 415);
+    }
+}
+
+function &get_audios_meta() {
+    static $_data = null;
+
+    if ($_data === null) {
+        $data_text = file_get_contents(APP_ROOT . '/core/metadata.json');
+        $_data = json_decode($data_text);
+        unset($data_text);
+    }
+
+    return $_data;
+}
+
+function update_dataset($updated_dataset) {
+    $json = json_encode($updated_dataset);
+    file_put_contents(APP_ROOT . '/core/metadata.json', $updated_dataset);
+}
