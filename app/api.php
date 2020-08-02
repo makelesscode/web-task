d556a6dc3dec275b36dd7c4a983a4b0dcb3c09b3
Andrey Kostenko <makelesscode@gmail.com>
2020-08-02 10:00:00 +0300

    Add php entry point

---
+++ app/api.php
@@ -0,0 +1,24 @@
+<?php
+
+define('APP_ROOT', __DIR__);
+
+require_once APP_ROOT . '/core/config.php';
+require_once APP_ROOT . '/core/functions.php';
+require_once APP_ROOT . '/core/handlers.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+
+$request_method = strtolower($_SERVER['REQUEST_METHOD']);
+
+if (!in_array($request_method, ['get', 'post', 'put'])) {
+    display_api_error(405, 'This request method is not allowed.', 405);
+}
+
+require_once APP_ROOT . "/core/handlers/{$request_method}.php";
+$handler_func = "handle_{$request_method}";
+
+// Everything's ok, run the handler
+$api_result = $handler_func();
+
+echo json_encode($api_result);
