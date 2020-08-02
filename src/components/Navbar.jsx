e44e13d44c27fc77fe3703135dd342288728e0cf
Andrey Kostenko <makelesscode@gmail.com>
2020-08-02 15:04:26 +0300

    Add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import FAIcon from './FAIcon';
+import store from '../helpers/store';
+
+function openUploader() {
+  store.dispatch();
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <div className="container">
+        <span className="navbar-brand" href="#">
+          <FAIcon icon="music" />
+          {' '}
+          Music App
+        </span>
+        <button className="navbar-toggler" type="button" aria-label="Toggle navigation">
+          <FAIcon icon="bars" />
+        </button>
+        <div className="collapse navbar-collapse">
+          <div className="ml-auto">
+            <button className="btn btn-outline-light my-2 my-sm-0" type="button" onClick={openUploader}>Upload</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/components/common/Page.jsx
+++ src/components/common/Page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AudioListContainer from '../../containers/AudioListContainer';
 import TooltipConnected from '../../containers/ConnectedTooltip';
+import Navbar from '../Navbar';
 
 function Page() {
   return (
     <>
+      <Navbar />
       <AudioListContainer />
       <TooltipConnected />
     </>
