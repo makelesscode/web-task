4bddd6463fa1d204359140b9b13a77ca23290708
Andrey Kostenko <makelesscode@gmail.com>
2020-08-02 15:10:10 +0300

    Uploader modal show/hide logic

---
+++ src/actions/uploader.js
@@ -0,0 +1,4 @@
+import { createAction } from '@reduxjs/toolkit';
+
+export const show = createAction('uploader/show');
+export const hide = createAction('uploader/hide');
--- src/reducers/uploader.js
+++ src/reducers/uploader.js
@@ -1,8 +1,16 @@
 import { createReducer } from '@reduxjs/toolkit';
+import { show, hide } from '../actions/uploader';
 
 export const uploaderState = {
   visible: false,
   items: [],
 };
 
-export const uploader = createReducer(uploaderState, {});
+export const uploader = createReducer(uploaderState, {
+  [show]: (state) => {
+    state.visible = true;
+  },
+  [hide]: (state) => {
+    state.visible = false;
+  },
+});
