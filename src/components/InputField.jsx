755d04e33a24601b95936525be44a7fa72836691
Andrey Kostenko <makelesscode@gmail.com>
2020-08-03 09:11:36 +0300

    Add tracking input changes via onChange prop

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 class InputField extends React.Component {
   constructor(props) {
     super(props);
     const { value } = this.props;
+    this.onChange = this.onChange.bind(this);
     this.state = {
       value,
     };
   }
 
   onChange(evt) {
+    const { onChange } = this.props;
     this.setState({
       value: evt.target.value,
     });
+    onChange(evt.target.value);
   }
 
   render() {
-    const { type, className } = this.props;
+    const { type, className, ...restProps } = this.props;
     const { value } = this.state;
     return (
-      <input type={type} value={value} className={className} />
+      // eslint-disable-next-line
+      <input {...restProps} className={cx('form-control', className)} type={type} value={value} onChange={this.onChange} />
     );
   }
 }
 
 InputField.propTypes = {
+  onChange: PropTypes.func.isRequired,
   type: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   className: PropTypes.string,
