47f1205bf86d8d4e27c3fba2748632842e9da587
Andrey Kostenko <makelesscode@gmail.com>
2020-08-03 11:54:10 +0300

    Add helper function to convert durations to number, e.g. '01:45:38' -> 6338

--- src/helpers/duration.js
+++ src/helpers/duration.js
@@ -5,3 +5,8 @@ export function getDurationFromSeconds(duration) {
 export function getDurationLeft(secsTotal, secsLeft) {
   return `-${getDurationFromSeconds(secsTotal - secsLeft)}`;
 }
+
+export function getSecondsFromDuration(duration) {
+  const parts = duration.split(':');
+  return parts[0] * 60 * 60 + parts[1] * 60 + parseInt(parts[2], 10);
+}
